a796517c28b43ef934e6a805836f968e52bb1193
Aaron Ausserhofer <aaronausserhofer@gmail.com>
2024-10-15 20:20:00 +0200

    feat: generate BaseViewPage with IPage Interface

---
+++ ElektroWasserer.Web/src/BaseViewPage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Cms.Web.Common.Views;
+
+namespace ElektroWasserer.Web
+{
+    public abstract class EwUmbracoViewPage : EwUmbracoViewPage<IPage>
+    {
+    }
+
+    public abstract class EwUmbracoViewPage<TModel> : UmbracoViewPage<TModel>
+    {
+
+        [RazorInject]
+        public IHttpContextAccessor HttpContextAccessor { get; set; } = null!;
+    }
+}
