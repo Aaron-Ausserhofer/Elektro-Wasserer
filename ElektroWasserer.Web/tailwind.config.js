c0a56750a4c7c14152bce52967856a9a8787370c
Aaron Ausserhofer <aaronausserhofer@gmail.com>
2024-10-15 20:23:28 +0200

    chore: tailwind themes

--- ElektroWasserer.Web/tailwind.config.js
+++ ElektroWasserer.Web/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./Views/**/*.{cshtml,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': {
+                    50: "#fbffe7",
+                    100: "#f4ffc1",
+                    200: "#eeff86",
+                    300: "#ecff41",
+                    400: "#f2ff0d",
+                    500: "#ffff00",
+                    600: "#d1bf00",
+                    700: "#a68b02",
+                    800: "#896c0a",
+                    900: "#74580f",
+                    950: "#443004"
+                }
+            }
+        },
     },
     plugins: [],
 }
