27766c5f075cc291ff6266e42895c9b68d1a7864
Aaron-Ausserhofer <aaronausserhofer@gmail.com>
2024-10-25 10:30:50 +0200

    feat(partials): update styling

--- ElektroWasserer.Web/tailwind.config.js
+++ ElektroWasserer.Web/tailwind.config.js
@@ -17,8 +17,17 @@ module.exports = {
                     900: "#74580f",
                     950: "#443004"
                 }
+            },
+            keyframes: {
+                bounce: {
+                    '0%, 100%': { transform: 'translateY(-25%)', 'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)' },
+                    '50%': { transform: 'translateY(0)', 'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)' },
+                },
+            },
+            animation: {
+                'bounce-slow': 'bounce 5000ms infinite',
             }
-        },
+        }
     },
     plugins: [],
 }
