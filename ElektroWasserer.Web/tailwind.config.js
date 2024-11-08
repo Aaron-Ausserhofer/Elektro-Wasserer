3d67aeaf5c76343c95697a0ea59c4d5a0de41069
Aaron-Ausserhofer <aaronausserhofer@gmail.com>
2024-11-08 14:02:45 +0100

    feat(tailwind): custom styles

--- ElektroWasserer.Web/tailwind.config.js
+++ ElektroWasserer.Web/tailwind.config.js
@@ -16,6 +16,19 @@ module.exports = {
                     800: "#896c0a",
                     900: "#74580f",
                     950: "#443004"
+                },
+                'darkgray': {
+                    50: "#f6f6f6",
+                    100: "#e7e7e7",
+                    200: "#d1d1d1",
+                    300: "#b0b0b0",
+                    400: "#888888",
+                    500: "#6d6d6d",
+                    600: "#5d5d5d",
+                    700: "#4f4f4f",
+                    800: "#454545",
+                    900: "#3d3d3d",
+                    950: "#1a1a1a"
                 }
             },
             keyframes: {
@@ -23,9 +36,14 @@ module.exports = {
                     '0%, 100%': { transform: 'translateY(-25%)', 'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)' },
                     '50%': { transform: 'translateY(0)', 'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)' },
                 },
+                fadeIn: {
+                    from: { opacity: 0 },
+                    to: { opacity: 1 }
+                }
             },
             animation: {
                 'bounce-slow': 'bounce 5000ms infinite',
+                fade: 'fadeIn .5s ease-in-out',
             }
         }
     },
